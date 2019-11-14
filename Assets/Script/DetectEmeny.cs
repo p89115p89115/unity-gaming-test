b68b50e22688ca68a6fde5a372884cff1cfd3af4
jacky <a1261331@gmail.com>
2019-11-14 17:22:03 +0800

    detect test

---
+++ Assets/Script/DetectEmeny.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEditor;
+//需導入Editor
+public class DetectEmeny : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetEnemy();
+    }
+
+    public GameObject enemy;
+    public float radius = 1;
+    public void GetEnemy()
+    {
+
+
+
+        //球形射線檢測,得到半徑radius米範圍內所有的物件
+        Collider2D[] cols = Physics2D.OverlapCircleAll(this.transform.position, 3);
+
+        //判斷檢測到的物件中有沒有Enemy
+        if (cols.Length > 0)
+            for (int i = 0; i < cols.Length; i++)
+                if (cols[i].tag.Equals("Enemy"))
+                {
+                    print("Enemy's name is " + cols[i].name);
+                   
+                }
+    
+       
+    }
+    public float distanceToMe;           //智能体到目标的距离
+    public GameObject me;                //目标角色
+    public float isSeekDistance = 10.0f;  //可靠近范围
+    public int state;  
+
+
+    void Idle()
+    {
+        //获取两者间的距离
+        distanceToMe = Vector3.Distance(me.transform.position, this.transform.position);
+        if (distanceToMe > isSeekDistance) //大于可靠近范围，进入空闲状态
+        {
+            print("大於");
+        }
+        else
+        {
+            print("小於");
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        //要兩邊都剛體?
+        if (col.gameObject.tag == "Enemy")
+        {
+            print("Collision Enemy!!!!!");
+        }
+    }
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Enemy")
+        {
+            print("Trigger Enemy!!!!!");
+
+           
+
+        }
+    }
+
+}
+
+
+
+
+
---
+++ Assets/Script/move.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour
+{
+
+    public float movespeed;
+    //移動速度
+  
+    void Update()
+    {
+        float movementH = 0f;
+        float movementV = 0f;
+        movementH = Input.GetAxis("Horizontal") * movespeed * Time.deltaTime;
+        movementV = Input.GetAxis("Vertical") * movespeed * Time.deltaTime;
+        Vector3 newPos = new Vector3(transform.position.x + movementH, transform.position.y + movementV, transform.position.z);
+        transform.position = newPos;
+    }
+}
